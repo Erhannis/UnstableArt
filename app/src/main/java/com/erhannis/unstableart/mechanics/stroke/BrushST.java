2a302633a5e930ebafec4f0edcf6233e12f124c1
erhannis <Eyeillus@gmail.com>
2017-04-25 02:22:17 -0400

    Undid stroke rounding

--- app/src/main/java/com/erhannis/unstableart/mechanics/stroke/BrushST.java
+++ app/src/main/java/com/erhannis/unstableart/mechanics/stroke/BrushST.java
@@ -3,6 +3,7 @@ package com.erhannis.unstableart.mechanics.stroke;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 
 import com.erhannis.unstableart.mechanics.color.Color;
 import com.erhannis.unstableart.mechanics.color.DoublesColor;
@@ -14,8 +15,6 @@ public class BrushST extends StrokeTool {
   //TODO Hmm, tranforms?  Color bounds?  Contexts?  Paths?  Base thickness?
   public void apply(Color color, double size, Stroke stroke, Bitmap canvas) {
     Paint paint = new Paint();
-    paint.setStrokeCap(Paint.Cap.ROUND);
-    paint.setStrokeJoin(Paint.Join.ROUND);
     //TODO Set color, alpha, width
     //TODO Should maybe Canvas be passed in?
     Canvas cCanvas = new Canvas(canvas);
